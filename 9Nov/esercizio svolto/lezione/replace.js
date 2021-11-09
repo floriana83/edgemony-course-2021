ec41bade6e0aebbf9deb397f3381c7307ef75895
Floriana Domianello <floriana.domianello@gmail.com>
2021-11-09 17:12:02 +0100

    esercizi 1 e 2 completati

---
+++ 9Nov/esercizio svolto/lezione/replace.js
@@ -0,0 +1,28 @@
+
+function minimalCalc(numbers) {
+  const sum = () => numbers.reduce((acc, num) => acc + num);
+  const sub = () => numbers.reduce((acc, num) => acc - num);
+  const mult = () => numbers.reduce((acc, num) => acc * num);
+  const div = () => numbers.reduce((acc, num) => acc / num);
+  // potenza base [0] exp [1] 
+  // const pow = () => numbers.map(num => Math.pow(num,numbers[1]));
+ // eleva al quadrato gli elementi dell'array
+ 
+  const pow = () => numbers.map(num => Math.pow(num,2));
+  // const pow = () => numbers.map(num => num * num);
+
+
+  const sqrt = () => numbers.map(num => Math.sqrt (num));
+
+  return {
+    sum: sum(),
+    sub: sub(),
+    mult: mult(),
+    div: div(),
+    pow: pow(),
+    sqrt: sqrt()
+  };
+
+
+}
+
---
+++ 9Nov/esercizio svolto/lezione/script.js
@@ -0,0 +1,47 @@
+/////////////////// primo esercizio calcolatrice semplice
+
+
+function calculator(numbers) {
+  function sum() {
+    let sumTotal = 0;
+    for (num of numbers) sumTotal += num;
+    return sumTotal;
+  } 
+
+  function sub() {
+    let subTotal = numbers[0];
+    for (num of numbers) subTotal -= num;
+    return subTotal + numbers[0]; // senza aggiungere numbers [0] l'operazione darebbe: a-b-a con una ripetizione del primo numero, motivo per il quale per annullare l'operazione "doppia" aggiungiamo nuovamente il numero sottratto.
+  };
+
+  function mult() {
+    let multTotal = numbers[0];
+    for (num of numbers) multTotal *= num;
+    return multTotal / numbers[0];// senza aggiungere numbers [0] l'operazione darebbe: a*b*a con una ripetizione del primo numero, motivo per il quale per annullare l'operazione "doppia" dividiamo nuovamente il numero inserito.
+  }
+  // oppure moltiplico per 1 dal momento che ogni numero moltiplicato 1 da di nuovo se stesso.
+
+  // function mult() {
+  //   let multTotal = 1;
+  //   for (num of numbers) multTotal *= num;
+  //   return multTotal;
+  // }
+
+  function div() {
+    let divTotal = numbers[0];
+    for (num of numbers) divTotal /= num;
+    return divTotal * numbers[0];// senza aggiungere numbers [0] l'operazione darebbe: a/b/a con una ripetizione del primo numero, motivo per il quale per annullare l'operazione "doppia" moltiplichiamo nuovamente il numero inserito.
+  }
+
+
+  return {
+    sum: sum(),
+    sub: sub(),
+    mult: mult(),
+    div: div(),
+
+  };
+
+}
+
+/////////////////// calcolatrice con replace
